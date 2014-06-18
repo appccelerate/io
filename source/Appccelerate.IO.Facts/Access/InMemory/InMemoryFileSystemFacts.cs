57847641620301575fd57b85bd4b4878c20753d2
Urs Enzler <urs.enzler@gmail.com>
2014-06-18 15:18:15 +0200

    added InMemoryFileSystem

---
+++ source/Appccelerate.IO.Facts/Access/InMemory/InMemoryFileSystemFacts.cs
@@ -0,0 +1,282 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InMemoryFileSystemFacts.cs" company="Appccelerate">
+//   Copyright (c) 2008-2014
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Appccelerate.IO.Access.InMemory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using FluentAssertions;
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class InMemoryFileSystemFacts
+    {
+        private const string Folder = "c:\\folder";
+        private const string FileInFolder = Folder + "\\file.ext";
+        private const string OtherFolder = "c:\\otherfolder";
+        private const string OtherFileInOtherFolder = OtherFolder + "\\otherfile.ext";
+
+        private static readonly byte[] DummyFile = { 1, 2, 3 };
+
+        public class DirectoryFacts
+        {
+            private readonly InMemoryFileSystem testee;
+
+            public DirectoryFacts()
+            {
+                this.testee = new InMemoryFileSystem();
+            }
+
+            [Fact]
+            public void IsInitiallyEmpty()
+            {
+                this.testee.GetFilesOfRecursive("\\")
+                    .Should().BeEmpty();
+            }
+
+            [Fact]
+            public void CreatesADirectory()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.DirectoryExists(Folder)
+                    .Should().BeTrue();
+            }
+
+            [Fact]
+            public void SkipsCreatingADirectory_WhenDirectoryAlreadyExists()
+            {
+                this.testee.CreateDirectory(Folder);
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.DirectoryExists(Folder)
+                    .Should().BeTrue();
+            }
+
+            [Theory]
+            [InlineData(Folder, Folder, true)]
+            [InlineData(Folder, "c:\\other", false)]
+            public void ReturnsWhetherADirectoryExists(string create, string query, bool expected)
+            {
+                this.testee.CreateDirectory(create);
+
+                bool result = this.testee.DirectoryExists(query);
+
+                result.Should().Be(expected);
+            }
+
+            [Fact]
+            public void DeletesADirectory()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.DeleteDirectory(Folder);
+
+                this.testee.DirectoryExists(Folder)
+                    .Should().BeFalse();
+            }
+
+            [Fact]
+            public void ThrowsDirectoryNotFoundExceptionOnDeletingADirectory_WhenDirectoryDoesNotExist()
+            {
+                Action action = () => this.testee.DeleteDirectory(Folder);
+
+                action.ShouldThrow<DirectoryNotFoundException>();
+            }
+        }
+
+        public class FileFacts
+        {
+            private readonly InMemoryFileSystem testee;
+
+            public FileFacts()
+            {
+                this.testee = new InMemoryFileSystem();
+            }
+
+            [Fact]
+            public void AddsAFile()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.AddFile(FileInFolder, DummyFile);
+
+                this.testee.FileExists(FileInFolder)
+                    .Should().BeTrue();
+            }
+
+            [Fact]
+            public void AddsAFile_WhenFileAlreadyExists()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.AddFile(FileInFolder, new byte[] { 9, 9 });
+                this.testee.AddFile(FileInFolder, DummyFile);
+
+                this.testee.GetFile(FileInFolder)
+                    .Should().Equal(DummyFile);
+            }
+
+            [Fact]
+            public void ThrowsDirectoryNotFoundExceptionOnAddingAFile_WhenDirectoryDoesNotYetExist()
+            {
+                Action action = () => this.testee.AddFile(FileInFolder, DummyFile);
+
+                action.ShouldThrow<DirectoryNotFoundException>();
+            }
+
+            [Fact]
+            public void DeletesAFile()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.AddFile(FileInFolder, DummyFile);
+
+                this.testee.DeleteFile(FileInFolder);
+
+                this.testee.FileExists(FileInFolder)
+                    .Should().BeFalse();
+            }
+
+            [Fact]
+            public void KeepsTheDirectoryWhenDeletingAFile()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.AddFile(FileInFolder, DummyFile);
+
+                this.testee.DeleteFile(FileInFolder);
+
+                this.testee.DirectoryExists(Folder)
+                    .Should().BeTrue();
+            }
+
+            [Fact]
+            public void SkipsDeletingAFile_WhenDirectoryExistsButFileDoesNotExist()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                this.testee.DeleteFile(FileInFolder);
+
+                this.testee.FileExists(FileInFolder)
+                    .Should().BeFalse();
+            }
+
+            [Fact]
+            public void ThrowsDirectoryNotFoundExceptionOnDeletingAFile_WhenDirectoryDoesNotExist()
+            {
+                Action action = () => this.testee.DeleteFile(FileInFolder);
+
+                action.ShouldThrow<DirectoryNotFoundException>();
+            }
+
+            [Fact]
+            public void MovesAFile()
+            {
+                this.testee.CreateDirectory(Folder);
+                this.testee.AddFile(FileInFolder, DummyFile);
+                this.testee.CreateDirectory(OtherFolder);
+
+                this.testee.Move(FileInFolder, OtherFileInOtherFolder);
+
+                this.testee.FileExists(OtherFileInOtherFolder)
+                    .Should().BeTrue();
+            }
+
+            [Fact]
+            public void ThrowsFileNotFoundExceptionOnMovingAFile_WhenFileDoesNotExist()
+            {
+                this.testee.CreateDirectory(Folder);
+                this.testee.CreateDirectory(OtherFolder);
+
+                Action action = () => this.testee.Move(FileInFolder, OtherFileInOtherFolder);
+
+                action.ShouldThrow<FileNotFoundException>();
+            }
+
+            [Fact]
+            public void ThrowsDirectoryNotFoundExceptionOnMovingAFile_WhenSourceFolderDoesNotExist()
+            {
+                this.testee.CreateDirectory(OtherFolder);
+
+                Action action = () => this.testee.Move(FileInFolder, OtherFileInOtherFolder);
+
+                action.ShouldThrow<DirectoryNotFoundException>();
+            }
+
+            [Fact]
+            public void MovesAFile_WhenDestinationFolderDoesNotExist()
+            {
+                this.testee.CreateDirectory(Folder);
+                this.testee.AddFile(FileInFolder, DummyFile);
+
+                this.testee.Move(FileInFolder, OtherFileInOtherFolder);
+
+                this.testee.FileExists(OtherFileInOtherFolder)
+                    .Should().BeTrue();
+
+                this.testee.DirectoryExists(OtherFolder)
+                    .Should().BeTrue();
+            }
+
+            [Fact]
+            public void MovesAFile_WhenAlreadyAFileAtTargetExists()
+            {
+                this.testee.CreateDirectory(Folder);
+                this.testee.AddFile(FileInFolder, DummyFile);
+                this.testee.CreateDirectory(OtherFolder);
+                this.testee.AddFile(OtherFileInOtherFolder, new byte[] { 9, 9 });
+
+                this.testee.Move(FileInFolder, OtherFileInOtherFolder);
+
+                this.testee.GetFile(OtherFileInOtherFolder)
+                    .Should().Equal(DummyFile);
+            }
+
+            [Fact]
+            public void GetsAFile()
+            {
+                this.testee.CreateDirectory(Folder);
+                this.testee.AddFile(FileInFolder, DummyFile);
+
+                IEnumerable<byte> result = this.testee.GetFile(FileInFolder);
+
+                result.Should().Equal(DummyFile);
+            }
+
+            [Fact]
+            public void ThrowsAnFileNotFoundExceptionOnGettingAFile_WhenFileDoesNotExist()
+            {
+                this.testee.CreateDirectory(Folder);
+
+                Action action = () => this.testee.GetFile(FileInFolder);
+
+                action.ShouldThrow<FileNotFoundException>();
+            }
+
+            [Fact]
+            public void ThrowsAnFileNotFoundExceptionOnGettingAFile_WhenDirectoryDoesNotExist()
+            {
+                Action action = () => this.testee.GetFile(FileInFolder);
+
+                action.ShouldThrow<DirectoryNotFoundException>();
+            }
+        }
+    }
+}
---
+++ source/Appccelerate.IO/Access/InMemory/InMemoryFile.cs
@@ -0,0 +1,313 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InMemoryFile.cs" company="Appccelerate">
+//   Copyright (c) 2008-2014
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Appccelerate.IO.Access.InMemory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Security.AccessControl;
+    using System.Text;
+
+    public class InMemoryFile : IFile
+    {
+        private readonly IInMemoryFileSystem fileSystem;
+
+        public InMemoryFile(IInMemoryFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public void Delete(string path)
+        {
+            this.fileSystem.DeleteFile(path);
+        }
+
+        public bool Exists(string path)
+        {
+            return this.fileSystem.FileExists(path);
+        }
+
+        public IEnumerable<byte> ReadAllBytes(string path)
+        {
+            return this.fileSystem.GetFile(path);
+        }
+
+        public string ReadAllText(string path)
+        {
+            return Encoding.Default.GetString(this.ReadAllBytes(path).ToArray());
+        }
+
+        public void WriteAllBytes(string path, IEnumerable<byte> bytes)
+        {
+            this.fileSystem.CreateDirectory(Path.GetDirectoryName(path));
+            this.fileSystem.AddFile(path, bytes);
+        }
+
+        public void WriteAllText(string path, string contents)
+        {
+            this.WriteAllBytes(path, Encoding.Default.GetBytes(contents));
+        }
+
+        public Stream OpenRead(string path)
+        {
+            var bytes = this.fileSystem.GetFile(path).ToArray();
+
+            var stream = new MemoryStream(bytes);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            return stream;
+        }
+
+        public void Move(string sourceFileName, string destinationFileName)
+        {
+            this.fileSystem.Move(sourceFileName, destinationFileName);
+        }
+
+        public void Copy(string sourceFileName, string destFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Copy(string sourceFileName, string destFileName, bool overwrite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StreamWriter CreateText(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileAttributes GetAttributes(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLastWriteTime(string path, DateTime lastWriteTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAttributes(string path, FileAttributes fileAttributes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> ReadAllLines(string path, Encoding encoding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> ReadAllLines(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> ReadLines(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> ReadLines(string path, Encoding encoding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadAllText(string path, Encoding encoding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAllLines(string path, IEnumerable<string> contents, Encoding encoding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAllLines(string path, IEnumerable<string> contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAllText(string path, string contents, Encoding encoding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Open(string path, FileMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Open(string path, FileMode mode, FileAccess access)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Open(string path, FileMode mode, FileAccess access, FileShare share)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppendAllLines(string path, IEnumerable<string> contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppendAllLines(string path, IEnumerable<string> contents, Encoding encoding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppendAllText(string path, string contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppendAllText(string path, string contents, Encoding encoding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StreamWriter AppendText(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Create(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Create(string path, int bufferSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Create(string path, int bufferSize, FileOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Create(string path, int bufferSize, FileOptions options, FileSecurity fileSecurity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Decrypt(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Encrypt(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileSecurity GetAccessControl(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileSecurity GetAccessControl(string path, AccessControlSections includeSections)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCreationTime(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCreationTimeUtc(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetLastAccessTime(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetLastAccessTimeUtc(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetLastWriteTime(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetLastWriteTimeUtc(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StreamReader OpenText(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream OpenWrite(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Replace(string sourceFileName, string destinationFileName, string destinationBackupFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Replace(string sourceFileName, string destinationFileName, string destinationBackupFileName, bool ignoreMetadataErrors)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAccessControl(string path, FileSecurity fileSecurity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCreationTime(string path, DateTime creationTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCreationTimeUtc(string path, DateTime creationTimeUtc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLastAccessTime(string path, DateTime lastAccessTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLastAccessTimeUtc(string path, DateTime lastAccessTimeUtc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLastWriteTimeUtc(string path, DateTime lastWriteTimeUtc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
